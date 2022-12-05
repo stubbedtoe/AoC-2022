6021c3d9fb623181c864670f4dd6db5c69e5504c
Andrew Healy <andrewhealy@deloitte.co.uk>
2022-12-05 10:20:35 +0000

    solve day4

--- src/Solutions/Day4/Input.elm
+++ src/Solutions/Day4/Input.elm
@@ -7,17 +7,22 @@ module Solutions.Day4.Input exposing (..)
 
 testInput : String
 testInput =
-    "replace me with the test input"
+    """"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
 
 
 expectedA : String
 expectedA =
-    ""
+    "2"
 
 
 expectedB : String
 expectedB =
-    ""
+    "4"
 
 
 input : String
--- src/Solutions/Day4/Solution.elm
+++ src/Solutions/Day4/Solution.elm
@@ -5,16 +5,72 @@ module Solutions.Day4.Solution exposing (..)
 
 
 import Solutions.Day4.Input as Input
+import Parser exposing (Parser, (|.), (|=), succeed, int, symbol)
+import Result
+import Set exposing (Set)
+
+type alias ElfPair = 
+    { lower1 : Int
+    , upper1 : Int
+    , lower2 : Int
+    , upper2 : Int
+    }
+
+type alias ElfSets = (Set Int, Set Int)
+
+pairsToSets : ElfPair -> ElfSets
+pairsToSets { lower1, upper1, lower2, upper2 } =
+    (Set.fromList (List.range lower1 upper1)
+    , Set.fromList (List.range lower2 upper2))
+
+overlapAtAll : ElfSets -> Bool
+overlapAtAll (set1, set2) =
+    Set.intersect set1 set2
+        |> Set.size
+        |> (<) 0
+
+isContainedWithinTheOther : ElfPair -> Bool
+isContainedWithinTheOther { lower1, upper1, lower2, upper2 } =
+    if lower1 < lower2 then
+        upper2 <= upper1
+    else if lower2 < lower1 then
+        upper1 <= upper2
+    else
+        True
+
+parseElfPair : Parser ElfPair
+parseElfPair =
+    succeed ElfPair
+        |= int
+        |. symbol "-"
+        |= int
+        |. symbol ","
+        |= int
+        |. symbol "-"
+        |= int
+
+
+parseLines : String -> List ElfPair
+parseLines input =
+    String.lines input
+        |> List.filterMap (\s -> Parser.run parseElfPair s |> Result.toMaybe)
 
 
 solveA : String -> String
 solveA input =
-    "implement partA here"
+    parseLines input
+    |> List.filter isContainedWithinTheOther
+    |> List.length
+    |> String.fromInt
 
 
 solveB : String -> String
 solveB input =
-    "implement partB here"
+    parseLines input
+    |> List.map pairsToSets
+    |> List.filter overlapAtAll
+    |> List.length
+    |> String.fromInt
 
 
 partA : String -> String
