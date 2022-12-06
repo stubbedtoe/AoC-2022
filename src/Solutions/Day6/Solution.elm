c7a261102945e65a1ed5714d1a26c542213a7e92
Andrew Healy <andrewhealy@deloitte.co.uk>
2022-12-06 06:28:19 +0000

    remove duplicated code

--- src/Solutions/Day6/Solution.elm
+++ src/Solutions/Day6/Solution.elm
@@ -7,7 +7,7 @@ module Solutions.Day6.Solution exposing (..)
 import Solutions.Day6.Input as Input
 import Set
 import List.Extra exposing (Step(..))
-import Solutions.Day1.Solution exposing (solveA)
+import Solutions.Day1.Solution exposing (..)
 
 containsRepeatedChar : List Char -> Bool
 containsRepeatedChar chars =
@@ -18,31 +18,16 @@ containsRepeatedChar chars =
     Set.size set /= List.length chars
 
 
-solveBInner : List Char -> Int -> Int
-solveBInner chars index =
-    if (containsRepeatedChar (List.take 14 chars)) then
+solveInner : List Char -> Int -> Int -> Int
+solveInner chars index take =
+    if (containsRepeatedChar (List.take take chars)) then
         let
             rest =
                 (List.tail chars)
         in
         case rest of
             Just r ->
-                solveBInner r (index + 1)
-            Nothing ->
-                index
-    else
-        index
-
-solveAInner : List Char -> Int -> Int
-solveAInner chars index =
-    if (containsRepeatedChar (List.take 4 chars)) then
-        let
-            rest =
-                (List.tail chars)
-        in
-        case rest of
-            Just r ->
-                solveAInner r (index + 1)
+                solveInner r (index + 1) take
             Nothing ->
                 index
     else
@@ -50,13 +35,13 @@ solveAInner chars index =
 
 solveA : String -> String
 solveA input =
-    solveAInner (String.toList input) 4
+    solveInner (String.toList input) 4 4
         |> String.fromInt
 
 
 solveB : String -> String
 solveB input =
-    solveBInner (String.toList input) 14
+    solveInner (String.toList input) 14 14
         |> String.fromInt
 
 
