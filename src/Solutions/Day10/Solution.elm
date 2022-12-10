6374711f288344612039e8693b75fad2effca70d
Andrew Healy <andrewhealy@deloitte.co.uk>
2022-12-10 10:48:29 +0000

    solve day10 partA

--- src/Solutions/Day10/Solution.elm
+++ src/Solutions/Day10/Solution.elm
@@ -5,11 +5,61 @@ module Solutions.Day10.Solution exposing (..)
 
 
 import Solutions.Day10.Input as Input
+import List.Extra exposing (cycle)
+
+type alias State =
+    { x : Int
+    , cycle : Int
+    , signalStrength : Int
+    }
+
+
+intitalState : State
+intitalState =
+    { x = 1
+    , signalStrength = 0
+    , cycle = 1 }
+
+
+
+add : Int -> State -> State
+add n { x, cycle, signalStrength } =
+    let
+        newSignal =
+            if (modBy 40 cycle) == 20 then
+                signalStrength + (x * cycle)
+            else
+                signalStrength
+    in
+    { signalStrength = newSignal
+    , cycle = cycle + 1
+    , x = x + n }
+
+
+parseLine : String -> List (State -> State)
+parseLine line =
+    if line == "noop" then
+        [ add 0 ]
+    else
+        case String.split " " line of
+            [ "addx", x ] ->
+                case String.toInt x of
+                    Just toAdd ->
+                        [ add 0, add toAdd ]
+                    Nothing ->
+                        []
+            _ ->
+                []
 
 
 solveA : String -> String
 solveA input =
-    "implement partA here"
+    String.lines input
+        |> List.map parseLine
+        |> List.concatMap identity
+        |> List.foldl (\fn state -> fn state) intitalState
+        |> .signalStrength
+        |> String.fromInt
 
 
 solveB : String -> String
