79bcace0836b0ec207e8981c5968e4055be9d6a1
Andrew Healy <andrewhealy@deloitte.co.uk>
2022-12-10 12:03:18 +0000

    solve day 10 partB

--- src/Solutions/Day10/Solution.elm
+++ src/Solutions/Day10/Solution.elm
@@ -11,6 +11,7 @@ type alias State =
     { x : Int
     , cycle : Int
     , signalStrength : Int
+    , screen : List String
     }
 
 
@@ -18,22 +19,47 @@ intitalState : State
 intitalState =
     { x = 1
     , signalStrength = 0
-    , cycle = 1 }
+    , cycle = 1
+    , screen = ["", "", "", "", "", ""] }
 
+
+drawScreen : State -> String
+drawScreen { screen } =
+    String.join "\n" screen
 
 
 add : Int -> State -> State
-add n { x, cycle, signalStrength } =
+add n { x, cycle, signalStrength, screen } =
     let
         newSignal =
-            if (modBy 40 cycle) == 20 then
+            if ((modBy 40 cycle) == 20 && cycle <= 220) then
                 signalStrength + (x * cycle)
             else
                 signalStrength
+
+        xCoord = 
+            (modBy 40 (cycle - 1))
+
+        currentRowNumber =
+            (cycle - 1) // 40
+
+        currentRow =
+            Maybe.withDefault "" (List.Extra.getAt currentRowNumber screen)
+
+        newPixel =
+            if List.member xCoord [ x - 1, x, x + 1 ] then
+                "#"
+            else
+                "."
+
+        newRow =
+            currentRow ++ newPixel
     in
+
     { signalStrength = newSignal
     , cycle = cycle + 1
-    , x = x + n }
+    , x = x + n
+    , screen = List.Extra.setAt currentRowNumber newRow screen }
 
 
 parseLine : String -> List (State -> State)
@@ -64,7 +90,11 @@ solveA input =
 
 solveB : String -> String
 solveB input =
-    "implement partB here"
+    String.lines input
+        |> List.map parseLine
+        |> List.concatMap identity
+        |> List.foldl (\fn state -> fn state) intitalState  
+        |> drawScreen
 
 
 partA : String -> String
